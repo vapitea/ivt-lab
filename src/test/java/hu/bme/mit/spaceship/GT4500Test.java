632dcd330884cf1c2d2bf24e046abde0916d62d0
Milan Andras Gyuricska <vapitea@gmail.com>
2020-05-21 12:49:14 +0200

    Use Mocks in Unit tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,40 +1,64 @@
 package hu.bme.mit.spaceship;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
-  private GT4500 ship;
+    private GT4500 ship;
+    private TorpedoStore mockTorpedoStore1;
+    private TorpedoStore mockTorpedoStore2;
 
-  @BeforeEach
-  public void init(){
-    this.ship = new GT4500();
-  }
 
-  @Test
-  public void fireTorpedo_Single_Success(){
-    // Arrange
+    @BeforeEach
+    public void init() {
 
-    // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+        mockTorpedoStore1 = mock(TorpedoStore.class);
+        mockTorpedoStore2 = mock(TorpedoStore.class);
 
-    // Assert
-    assertEquals(true, result);
-  }
+        this.ship = new GT4500(mockTorpedoStore1, mockTorpedoStore2);
+    }
 
-  @Test
-  public void fireTorpedo_All_Success(){
-    // Arrange
+    @Test
+    public void fireTorpedo_Single_Success() {
+        // Arrange
+        when(mockTorpedoStore1.getTorpedoCount()).thenReturn(10);
+        when(mockTorpedoStore2.getTorpedoCount()).thenReturn(10);
+        when(mockTorpedoStore1.isEmpty()).thenReturn(false);
+        when(mockTorpedoStore2.isEmpty()).thenReturn(false);
+        when(mockTorpedoStore1.fire(anyInt())).thenReturn(true);
+        when(mockTorpedoStore2.fire(anyInt())).thenReturn(true);
 
-    // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
 
-    // Assert
-    assertEquals(true, result);
-  }
+        // Act
+        ship.fireTorpedo(FiringMode.SINGLE);
+
+        // Assert
+        verify(mockTorpedoStore1, times(1)).fire(1);
+        verify(mockTorpedoStore2, times(0)).fire(1);
+
+    }
+
+    @Test
+    public void fireTorpedo_All_Success() {
+        // Arrange
+        when(mockTorpedoStore1.getTorpedoCount()).thenReturn(10);
+        when(mockTorpedoStore2.getTorpedoCount()).thenReturn(10);
+        when(mockTorpedoStore1.isEmpty()).thenReturn(false);
+        when(mockTorpedoStore2.isEmpty()).thenReturn(false);
+        when(mockTorpedoStore1.fire(anyInt())).thenReturn(true);
+        when(mockTorpedoStore2.fire(anyInt())).thenReturn(true);
+
+        // Act
+        ship.fireTorpedo(FiringMode.ALL);
+
+        // Assert
+        verify(mockTorpedoStore1, times(1)).fire(1);
+        verify(mockTorpedoStore2, times(1)).fire(1);
+
+    }
 
 }
